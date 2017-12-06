7358a875269e7ce99067b9e85c525bd7ad11192c
vKirillov <specctral@gmail.com>
2017-12-06 20:09:32 +0300

    pwd

--- src/pwd.c
+++ src/pwd.c
@@ -2,3 +2,20 @@
 // Created by slava on 05.12.17.
 //
 
+#include <memory.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    char *path = getenv("PWD");
+
+    if (!path || path[0] != '/')
+        exit(1);
+    while ((path = strstr(path, "/."))) {
+        if (!path[2] || path[2] == '/' || (path[2] == '.' && (!path[3] || path[3] == '/')))
+            exit(1);
+        path++;
+    }
+    puts(path);
+    exit(0);
+}
